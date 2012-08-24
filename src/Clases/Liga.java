683fa8e2732ec1671642560e759b6754489c53d0
Matias <matiasv2008@adinet.com.uy>
2012-08-24 14:32:24 -0300

    agregado a clase liga la lista de equipos y de partidos

--- src/Clases/Liga.java
+++ src/Clases/Liga.java
@@ -1,6 +1,12 @@
 package Clases;
 
+import java.util.List;
+
 public class Liga extends Competicion {
+    
+    private List<Equipo> equipos;
+    private List<Partido> partidos;
+    
     public Liga(String nombre){
         super(nombre);
     }
