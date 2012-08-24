abb54bff1475260a1701dfc839716eb30c009e24
Matias <matiasv2008@adinet.com.uy>
2012-08-24 14:45:03 -0300

    setters y getters para que jeank se ria

--- src/Clases/Liga.java
+++ src/Clases/Liga.java
@@ -10,4 +10,16 @@ public class Liga extends Competicion {
     public Liga(String nombre){
         super(nombre);
     }
+    public List<Equipo> getEquipos() {
+        return equipos;
+    }
+    public void setEquipos(List<Equipo> equipos) {
+        this.equipos = equipos;
+    }
+    public List<Partido> getPartidos() {
+        return partidos;
+    }
+    public void setPartidos(List<Partido> partidos) {
+        this.partidos = partidos;
+    }
 }
