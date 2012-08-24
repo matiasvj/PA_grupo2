78d796049ffa725ad4d337f9902712ab750d4aad
Matias <matiasv2008@adinet.com.uy>
2012-08-24 00:46:11 -0300

    actualizacion

--- src/Clases/ManejadorBD.java
+++ src/Clases/ManejadorBD.java
@@ -3,8 +3,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ManejadorBD {
     
